8bb85c477677526539084820cce87458f85d7679
omniboyOk <kachorritou@gmail.com>
2020-09-20 02:58:25 -0300

    files

--- src/App/Desktop/Content/shortcut.jsx
+++ src/App/Desktop/Content/shortcut.jsx
@@ -1,22 +1,40 @@
 import React from "react";
-import { FaRegFileAlt } from "react-icons/fa";
+import { FaRegFileAlt, FaRegFileImage, FaRegFile } from "react-icons/fa";
+import { motion } from "framer-motion";
 
+const icon = (type) => {
+  switch (type) {
+    case "image":
+      return (
+        <FaRegFileImage className="DesktopIcon text-white bg-dark rounded p-1" />
+      );
+    case "text":
+      return (
+        <FaRegFileAlt className="DesktopIcon text-white bg-dark rounded p-1" />
+      );
+    default:
+      return (
+        <FaRegFile className="DesktopIcon text-white bg-dark rounded p-1" />
+      );
+  }
+};
 function DesktopIcon(props) {
-  const { name, extension, route } = props;
+  const { name, extension, type } = props;
   return (
-    <div>
-      <FaRegFileAlt className="DesktopIcon text-white bg-dark rounded p-1" />
+    <motion.div drag dragMomentum={false}>
+      {icon(type)}
       <p>
         {name}.{extension}
       </p>
-    </div>
+    </motion.div>
   );
 }
 
 DesktopIcon.defaultProps = {
-    name: 'file',
-    extension: 'xxx',
-    route: ''
-}
+  name: "file",
+  extension: "xxx",
+  route: "",
+  type: "default",
+};
 
 export default DesktopIcon;
--- src/App/Desktop/Toolbar/toolbar.jsx
+++ src/App/Desktop/Toolbar/toolbar.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import ToolbarClock from "./clock";
+import { FaReact } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 function DesktopToolbar(props) {
   return (
     <Container fluid className="DesktopToolbar bg-dark">
       <Row>
-        <Col xs lg="2"></Col>
-        <Col></Col>
-        <Col xs lg="1">
-          <ToolbarClock/>
+        <Col xs="2" lg="1" className="p-0">
+          <div className="DesktopStart">
+            <motion.div whileHover={{ rotate: 45 }}>
+              <FaReact color="white" size="3em" />
+            </motion.div>
+          </div>
+        </Col>
+        <Col xs lg="10"></Col>
+        <Col xs="3" lg="1">
+          <ToolbarClock />
         </Col>
       </Row>
     </Container>
