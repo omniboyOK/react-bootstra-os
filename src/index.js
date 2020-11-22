d1f20a58dadb9bd7b1044f36d738b1ef17abe194
omniboyok <barrea.pablo@gmail.com>
2020-11-21 22:11:36 -0300

    Added Toastify

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById("root")
 );
