1e871aec8de005388a8bb15b042f0f6636696035
omniboyok <barrea.pablo@gmail.com>
2020-11-21 22:40:51 -0300

    [Feature] Added Markdown and Folder icon

--- src/App/@bootOS/utils/IconUtil.js
+++ src/App/@bootOS/utils/IconUtil.js
@@ -1,10 +1,14 @@
 module.exports = {
-    formatToIcon: extension => {
-      switch (extension) {
-        case "jpg":
-          return "/assets/icons/png/099-picture-2.png";
-        default:
-          return "/assets/icons/png/126-file-39.png";
-      }
+  formatToIcon: (extension) => {
+    switch (extension) {
+      case "jpg":
+        return "/assets/icons/png/099-picture-2.png";
+      case "md":
+        return "/assets/icons/png/191-file-7.png";
+      case "dir":
+        return "/assets/icons/png/136-folder-21.png";
+      default:
+        return "/assets/icons/png/126-file-39.png";
     }
-}
+  },
+};
